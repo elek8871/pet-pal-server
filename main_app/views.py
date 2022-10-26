f892ef19a0d4e62939e86a59f241d43820e331eb
elek8871 <elekmichelle@gmail.com>
2022-10-25 19:45:49 -0600

    connects django to psql db

--- main_app/views.py
+++ main_app/views.py
@@ -3,6 +3,13 @@ from django.shortcuts import render
 # Add the following import
 from django.http import HttpResponse
 
+#import models
+from .models import User
+
+
 # Define the home view
 def home(request):
   return HttpResponse('<h1> (╯°□°）╯︵ ┻━┻ </h1>')
+
+
+
